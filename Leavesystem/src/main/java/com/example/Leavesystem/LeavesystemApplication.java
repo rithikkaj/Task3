5092d323e73e8493e03322bdd35dce46163c1d35
Rithikka <jrithikka@gmail.com>
2026-03-26 10:33:09 +0530

    first commit

---
+++ Leavesystem/src/main/java/com/example/Leavesystem/LeavesystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Leavesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavesystemApplication.class, args);
+	}
+
+}
---
+++ Leavesystem/src/test/java/com/example/Leavesystem/LeavesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Leavesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
